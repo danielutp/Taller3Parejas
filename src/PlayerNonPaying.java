fcee81a7bb5d3ef1c27ca56b7ecb64fa381fcd19
danielgil <danistcruz@gmail.com>
2022-02-09 23:00:24 -0500

    Actualizacion de clases
    Se crean las nuevas clases PlayerNonPaying y PlayerPaid que heredan de player.

--- src/PlayerNonPaying.java
+++ src/PlayerNonPaying.java
@@ -1,6 +1,11 @@
 
 public class PlayerNonPaying extends Player {
 
-	
+	@Override
+	void player(Song song) {
+		System.out.println("reproduciendo : " + song.title);
+		System.out.println("reproduciendo Comerical!!!!");
+		
+	}
 
 }
--- src/PlayerPaid.java
+++ src/PlayerPaid.java
@@ -4,5 +4,25 @@ import javax.swing.JOptionPane;
 
 public class PlayerPaid extends Player {
 
-	
+	public PlayerPaid() {
+		super();
+	}
+
+	@Override
+	void player(Song song) {
+		System.out.println("reproduciendo : " + song.title);
+
+	}
+
+	public void player(PlayList songList) {
+		for (String songName : songList.tracklist) {
+
+			for (Song song : this.songLibrary.tracklist) {
+
+				if (songName.equals(song.title)) {
+					player(song);
+				}
+			}
+		}
+	}
 }
